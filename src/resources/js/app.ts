a3220372c1be7e9e92751bebb9044562b427ff96
ShineShipMark <astray503@gmail.com>
2026-02-08 14:26:14 +0900

    Pinia導入

--- src/resources/js/app.ts
+++ src/resources/js/app.ts
@@ -1,21 +1,28 @@
-import '../css/app.css';
+import "../css/app.css";
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import type { DefineComponent } from "vue";
+import { createApp, h } from "vue";
+import { createPinia } from "pinia";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => (title ? `${title} - ${appName}` : appName),
+  resolve: (name) =>
+    resolvePageComponent(
+      `./pages/${name}.vue`,
+      import.meta.glob<DefineComponent>("./pages/**/*.vue"),
+    ),
+  setup({ el, App, props, plugin }) {
+    const pinia = createPinia();
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(pinia)
+      .mount(el);
+  },
+  progress: {
+    color: "#4B5563",
+  },
 });

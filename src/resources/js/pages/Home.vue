cf4c805672c71223f4b11d4fc0bb57e99c1cf058
ShineShipMark <astray503@gmail.com>
2026-05-17 10:52:28 +0900

    ホーム画面簡易構築

--- src/resources/js/pages/Home.vue
+++ src/resources/js/pages/Home.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { Card } from '@/components/ui/card'
+import { Link } from '@inertiajs/vue3';
 </script>
 <template>
     <Card>
         ホーム画面
+        <Card>
+            <Link href="/input">収支入力</Link>
+            <Link href="/history">履歴</Link>
+            <Link href="/graph">グラフ</Link>
+        </Card>
     </Card>
 </template>

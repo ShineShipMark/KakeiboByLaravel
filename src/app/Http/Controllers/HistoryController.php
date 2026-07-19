beda1bd0f140fab84e9af29d35d6e53209bf03a4
ShineShipMark <astray503@gmail.com>
2026-07-19 16:11:52 +0900

    コントローラーの分割

---
+++ src/app/Http/Controllers/HistoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\DTO\FindExpenseDTO;
+use App\DTO\FindIncomeDTO;
+use App\DTO\UpdateExpenseDTO;
+use App\DTO\UpdateIncomeDTO;
+use App\Http\Requests\HistoryRequest;
+use App\UseCases\FindExpense;
+use App\UseCases\FindIncome;
+use App\UseCases\UpdateExpense;
+use App\UseCases\UpdateIncome;
+use App\UseCases\DeleteExpense;
+use App\UseCases\DeleteIncome;
+
+class HistoryController extends Controller
+{
+    public function getHistory(HistoryRequest $request, FindExpense $expenseUsecase, FindIncome $incomeUsecase) {
+        $expenditure = $request->input('expenditure', 'Expense');
+        if ($expenditure === 'Expense') {
+            $dataDTO=FindExpenseDTO::fromRequest($request);
+            $searchedData = $expenseUsecase->handle($dataDTO);
+        } else {
+            $dataDTO=FindIncomeDTO::fromRequest($request);
+            $searchedData = $incomeUsecase->handle($dataDTO);
+        }
+
+        return Inertia::render('History/', ['searchedData' => $searchedData]);
+    }
+
+    public function editData(int $id, HistoryRequest $request, UpdateExpense $expenseUsecase, UpdateIncome $incomeUsecase)
+    {
+        $expenditure = $request->input('expenditure', 'Expense');
+        
+        if ($expenditure === 'Expense') {
+            $updateDTO = UpdateExpenseDTO::fromRequest($id, $request);
+            $expenseUsecase->handle($updateDTO);
+        } else {
+            $updateDTO=UpdateIncomeDTO::fromRequest($id, $request);
+            $incomeUsecase->handle($updateDTO);
+        }
+
+        redirect()->action([HistoryController::class, 'history'])
+        ->with('message', '更新が完了しました');
+    }
+
+    public function deleteData(int $id, HistoryRequest $request, DeleteExpense $expenseUsecase, DeleteIncome $incomeUsecase) 
+    {
+        $expenditure = $request->input('expenditure', 'Expense');
+        
+        if ($expenditure === 'Expense') {
+            $expenseUsecase->handle($id);
+        } else {
+            $incomeUsecase->handle($id);
+        }
+
+        redirect()->action([HistoryController::class, 'history'])
+        ->with('message', '削除が完了しました');
+    }
+}
---
+++ src/app/Http/Controllers/InputController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\InputRequest;
+use App\DTO\RegisterExpenseDTO;
+use App\DTO\RegisterIncomeDTO;
+use App\UseCases\RegisterExpense;
+use App\UseCases\RegisterIncome;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class InputController extends Controller
+{
+    public function input()
+    {
+        return Inertia::render('Input');
+    }
+
+    public function store(InputRequest $request, RegisterExpense $expenseUsecase, RegisterIncome $incomeUsecase)
+    {
+        $expenditure = $request->input('expenditure', 'Expense');
+        
+        if ($expenditure === 'Expense') {
+            $inputDTO = RegisterExpenseDTO::fromRequest($request);
+            $expenseUsecase->handle($inputDTO);
+        } else {
+            $inputDTO=RegisterIncomeDTO::fromRequest($request);
+            $incomeUsecase->handle($inputDTO);
+        }
+
+        return Inertia::render('Input/');
+    }
+}

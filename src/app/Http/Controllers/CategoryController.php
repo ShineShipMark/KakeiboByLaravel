020a1e45a5e324665f30adb47cb5065bfb77665a
ShineShipMark <astray503@gmail.com>
2026-08-10 11:17:50 +0900

    Categoryのコントローラーに取得メソッド実装

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data\Category\CategoryResponseData;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+       $categories = Category::with('children')
+            ->whereNull('parent_id')
+            ->get();
+
+        return Inertia::render('Category/Index', [
+            'categories' => CategoryResponseData::collect($categories)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
